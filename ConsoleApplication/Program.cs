4ec66da9eb95300b27c76ad96d6a9c7496bc4e85
Bailee <rob16020@byui.edu>
2021-01-13 10:33:35 -0700

    Finished Project

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -23,11 +23,31 @@ namespace ConsoleApplication
             Console.WriteLine("The current date is " + currentDate.ToString("d"));
 
             // 5. 
+            DateTime now = DateTime.Now;
+            int day = now.DayOfYear;
+            int christmasTime = (365-6) - day;
+            Console.WriteLine("The number of days until Christmas is " + christmasTime);
 
-            Console.WriteLine("The number of days until Christmas is ");
+            // 6. 
+            double width, height, woodLength, glassArea;
+            string widthString, heightString;
 
+            Console.WriteLine("What is the width of the window?");
+            widthString = Console.ReadLine();
+            width = double.Parse(widthString);
 
+            Console.WriteLine("What is the height of the window?");
+            heightString = Console.ReadLine();
+            height = double.Parse(heightString);
 
+            woodLength = 2 * (width + height ) * 3.25;
+
+            glassArea = 2 * (width + height);
+
+            Console.WriteLine("The length of the wood is " + woodLength + " feet.");
+            Console.WriteLine("The area of the glass is " + glassArea + " square metres.");
+
+            // 7. 
             Console.WriteLine("Press Any Key to Exit");
             Console.ReadKey();
         }
